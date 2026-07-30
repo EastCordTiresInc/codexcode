889cb4640b39d7ec58a04b6640199fc13e9a0b58
EastCordTiresInc <wyvk4pfrdd@privaterelay.appleid.com>
2026-07-30 17:44:21 -0400

    Keep auth config free of cart script injection

--- auth-config.js
+++ auth-config.js
@@ -3,12 +3,3 @@ window.EASTCORD_AUTH_CONFIG = {
   supabaseUrl: '',
   supabaseAnonKey: '',
 };
-
-(() => {
-  if (window.EastCordCartIsolationLoaderLoaded) return;
-  window.EastCordCartIsolationLoaderLoaded = true;
-  const script = document.createElement('script');
-  script.src = 'cart-account-isolation-v2.js?v=2';
-  script.defer = true;
-  document.head.appendChild(script);
-})();
